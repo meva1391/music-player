43d34acc7bdbb966783c28e4ab7a4990d5998cad
meva <ebrahim.v.aghaie1391@gmail.com>
2021-07-04 15:20:07 +0430

    Music player

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TrackList from "./components/TrackList";
+import PlayerControls from "./components/PlayerControls";
+import { MusicPlayerProvider } from "./MusicPlayerContext";
+import 'bulma/css/bulma.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MusicPlayerProvider>
+      <div className="container">
+        <TrackList />
+        <PlayerControls />
+      </div>
+    </MusicPlayerProvider>
   );
 }
 
